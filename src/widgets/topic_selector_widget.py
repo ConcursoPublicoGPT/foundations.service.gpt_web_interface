56f4ee8ed91638cd8a1a53ac4941b280daae9add
joaolovatti <joaoplovatti@gmail.com>
2024-05-30 14:55:39 -0300

    new topic option

--- src/widgets/topic_selector_widget.py
+++ src/widgets/topic_selector_widget.py
@@ -7,7 +7,11 @@ class TopicSelectorWidget:
         st.write("")
         st.subheader("1. Tópicos")
         topic = st.selectbox(
-            "Escolha o tópico que você deseja estudar:", ["Administração Pública"]
+            "Escolha o tópico que você deseja estudar:",
+            [
+                "Administração Pública",
+                "Edital : Educação, Saúde, Desenvolvimento Social e Direitos Humanos",
+            ],
         )
 
         return topic

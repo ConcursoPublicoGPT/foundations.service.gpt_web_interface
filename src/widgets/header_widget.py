9a2f29814bfc7fd60b9c86d7817b00942aefc783
joaolovatti <joaoplovatti@gmail.com>
2024-07-05 00:50:10 -0300

    update to tr

--- src/widgets/header_widget.py
+++ src/widgets/header_widget.py
@@ -4,7 +4,7 @@ import streamlit as st
 class HeaderWidget:
     @staticmethod
     def display():
-        st.title(":orange[DSM-5 & CID-10] 📖")
+        st.title(":orange[DSM-5-TR & CID-10] 📖")
         st.write(
-            "Este BOT é capaz de utilizar referências do DSM-5 e do CID-10 para elobarar respostas inteligentes para as suas dúvidas. Faça as suas perguntas! 🧑‍🔬🔬"
+            "Este BOT é capaz de utilizar referências do DSM-5-TR e do CID-10 para elobarar respostas inteligentes para as suas dúvidas. Faça as suas perguntas! 🧑‍🔬🔬"
         )
